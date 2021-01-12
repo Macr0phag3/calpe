e228efb2cf7423ccc6dbc07c128cef6f3e85e7a4
wangbin11 <wangbin11@staff.weibo.com>
2021-01-12 11:59:31 +0800

    fix: 移除 etf 判断，直接获取基金的股票持仓

--- pe.py
+++ pe.py
@@ -53,13 +53,7 @@ def send(url, method='get', headers=None, params=None, data=None, json=False):
     return result
 
 
-def show(fund_name, fund_code, data, today_increase=0, is_etf=False):
-    fund_name = put_color(fund_name, "blue")
-    if is_etf:
-        print(f'\n[{fund_code}] {fund_name}')
-        print(f'  [-] {put_color("此为 ETF 基金，无法估算 PE", "gray")}')
-        return
-
+def show(fund_name, fund_code, data, stock_percent, today_increase=0):
     tb = pt(border=False, header=False)
     tb.field_names = ["股票名", "比例", "PE", "今日涨幅"]
     tb.align["股票名"] = "l"
@@ -71,7 +65,17 @@ def show(fund_name, fund_code, data, today_increase=0, is_etf=False):
         inc_color = 'green'
         tip = '↓'
 
+    fund_name = put_color(fund_name, "blue")
     print(f'\n[{fund_code}] {fund_name} {put_color(f"{tip} {today_increase}%", inc_color)}')
+    if stock_percent < 0.5:
+        too_low_tip = put_color(
+            f"此基金股票持仓总占比为 {round(stock_percent*100, 2)}%"
+            "，过低，PE 可能不准",
+            "gray"
+        )
+        print(
+            f'  [-] {too_low_tip}'
+        )
 
     cal_pe_rate = 0
     for d in data[:5]:
@@ -86,15 +90,37 @@ def show(fund_name, fund_code, data, today_increase=0, is_etf=False):
 
     print(tb)
     cal_pe_rate = round(cal_pe_rate, 2)
+    # 如果公司的市盈率超过30倍，
+    # 普遍被认为是价值高估区，也就是说可能收到的回报会低于3年期存款利率；
+    # 如果公司的市盈率超过了60倍，那可能就意味着市场泡沫的存在。
+    # 市盈率还在一定程度上体现了投资者对一家公司未来盈利能力的预期；
+    # 市盈率低也不一定是公司价值被低估，还可能是人们觉得这个公司的发展潜力有限。
     color = 'cyan'
-    if cal_pe_rate > 50:
+    if cal_pe_rate >= 30:
         color = 'yellow'
+    elif cal_pe_rate >= 60:
+        color = 'magenta'
 
     print(f'  [-] PE: {put_color(cal_pe_rate, color)}')
 
 
+def get_stock_percent(fund_code):
+    html = send(f'http://fundf10.eastmoney.com/zcpz_{fund_code}.html')
+    html = etree.HTML(html)
+    date = html.xpath(
+        '//*[@id="bodydiv"]/div[8]/div[3]/div[2]/div[3]/div/div[2]/div/table/tbody/tr[1]/td[1]/text()'
+    )[0]
+    stock_percent = html.xpath(
+        '//*[@id="bodydiv"]/div[8]/div[3]/div[2]/div[3]/div/div[2]/div/table/tbody/tr[1]/td[2]/text()'
+    )[0]
+
+    return date, float(stock_percent[:-1])/100
+
+
 @retry()
 def get_fund_info(fund_code):
+    date, stock_percent = get_stock_percent(fund_code)
+    year, month, _ = date.split('-')
     today_increase = round(float(re.findall(
         '<span id="fund_gszf" .+>(.+)%</span>',
         send(f'http://fundf10.eastmoney.com/ccmx_{fund_code}.html')
@@ -104,8 +130,8 @@ def get_fund_info(fund_code):
         'type': 'jjcc',
         'code': fund_code,
         'topline': '1000',
-        'year': '2020',
-        'month': '9',
+        'year': year,
+        'month': int(month),
         'rt': '0.07460008078625502',
     }
     html = send(
@@ -119,10 +145,6 @@ def get_fund_info(fund_code):
     html = etree.HTML(re.findall('var apidata={ content:"(.+)",arryear', html)[0])
 
     fund_name = html.xpath('/html/body/div[1]/div/h4/label[1]/a/text()')[0]
-    if 'etf' in fund_name.lower():
-        show(fund_name, fund_code, [], is_etf=True)
-        return
-
     if weird:
         secids = []
         root = '/html/body/div[1]/div/table/tbody/tr'
@@ -167,12 +189,12 @@ def get_fund_info(fund_code):
         stock_increase = float(stock['f3'])
         result.append([name, precent, pe_rate, stock_increase])
 
-    show(fund_name, fund_code, result, today_increase)
+    show(fund_name, fund_code, result, stock_percent, today_increase)
 
 
 print('[*] searching')
 
-fund_codes = ['161725', '003095', '008750', '320007', '001156']  # 基金代码
+fund_codes = ['161725', '003095', '008750', '001694', '001594', '320007', '001156']  # 基金代码
 for code in fund_codes:
     get_fund_info(code)
 
